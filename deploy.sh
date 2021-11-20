bfb9d8f7c8d2ffc62a9744ad92947dd8827f806f
Виталий Ещук <vitalik181276@gmail.com>
2021-11-20 22:49:00 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,8 @@ cd ${DEPLOY_DIR}
 docker login -u ${CI_REGISTRY_USER} -p ${CI_JOB_TOKEN} ${CI_REGISTRY}
 docker pull ${CI_REGISTRY_IMAGE}/curator:${CI_PIPELINE_ID}
 envsubst < docker-compose.tpl > docker-compose.yaml
-docker-compose down || true
-sleep 120
-docker-compose up -d
+cat docker-compose.yaml
+docker stack deploy --with-registry-auth --compose-file docker-compose.yaml elk
 
 cd
 rm -rf ${DEPLOY_DIR}
